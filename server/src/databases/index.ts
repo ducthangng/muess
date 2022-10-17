e9201d08c8fb3ae9dfbfa4c6c843aceaf90b9523
Nghi Hua <huanhatgianghi@gmail.com>
2022-10-17 21:14:38 +0700

    change db url to dynamic, change database from test to muessen

--- server/src/databases/index.ts
+++ server/src/databases/index.ts
@@ -1,7 +1,6 @@
 import { DB_USERNAME, DB_PASSWORD, DB_CLUSTER, DB_DATABASE } from '@config';
 
-console.log(DB_USERNAME, DB_PASSWORD, DB_CLUSTER, DB_DATABASE);
 export const dbConnection = {
-  url: `mongodb+srv://admin:Muessen@cluster0.7dou6.mongodb.net/?retryWrites=true&w=majority`,
+  url: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority`,
   options: {}
 };
