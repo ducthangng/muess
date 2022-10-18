8d65501bf0362aca1e31f78255baf704db8b3f85
root <root@LocCool.localdomain>
2022-10-18 13:21:02 +0700

    login page finished, waitting for backend implementation

--- ui/src/pages/Login/Login.tsx
+++ ui/src/pages/Login/Login.tsx
@@ -1,12 +1,17 @@
 // import { Form, Layout, Button, Input, Divider } from 'antd';
 // // import logo from '../../assets/logo-1.svg';
-// import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
+import {EyeInvisibleOutlined} from '@ant-design/icons';
 // import { useNavigate } from 'react-router-dom';
 import './Login.css';
 import LoginImage from '../../assets/images/login.png';
 import LoginLogo from '../../assets/images/logo.png';
 
 const Login = () => {
+    const [passwordShown, setPasswordShown] = useState(false);
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
     return (
         <>
             <div className='box-1'>
@@ -15,6 +20,19 @@ const Login = () => {
             </div>
             <div className='box-2'>
                 <div className='SignIn'>Sign In</div>
+                <div className='username'>Username/Email</div>
+                <div className='input_username'>
+                    <input></input>
+                </div>
+                <div className='password'>Password</div>
+                <div className='input_password'>
+                    <input type={passwordShown ? "text" : "password"} ></input>
+                    <EyeInvisibleOutlined className={passwordShown ? "shown" : "not"} onClick={togglePassword}/>
+                </div>
+                <div className='forgot_password'>Forgot password?</div>
+                <button className='signin_button'>SIGN IN</button>
+                <div className='dhaa'>Don't Have An Account?</div>
+                <div className='ca'>Create Account</div>
             </div>
         </>
     );
