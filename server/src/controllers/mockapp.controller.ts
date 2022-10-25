fb97aaf49d05157d00e37f7b787b3f6b4896adce
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-10-25 12:01:26 +0700

    update controller and interface

--- server/src/controllers/mockapp.controller.ts
+++ server/src/controllers/mockapp.controller.ts
@@ -3,20 +3,28 @@ import { CreateUserDto } from '@dtos/users.dto';
 import { App } from '@interfaces/apps.interface';
 //import appService from '@services/apps.service';
 
+const x: App = {
+  _id: '1',
+  title: '1',
+  description: '1',
+  rated: '1',
+  reviewer: '1',
+  appTag: '1',
+  downloaded: 1,
+  appInformations: '1',
+  feedbacks: [
+    {
+      name: '1',
+      content: '1'
+    }
+  ]
+};
 class AppController {
   //public appService = new appService();
 
   public getApps = async (req: Request, res: Response, next: NextFunction) => {
     try {
       // const findAllAppsData: App[] = await this.appService.findAllApp();
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // Name: abc
-      //     // Price: xyz
-      //   }
-      // ];
 
       //return
       res.status(200).json({ data: x, message: 'findAll' });
@@ -33,14 +41,6 @@ class AppController {
     try {
       //const appId: string = req.params.id;
       //const findOneAppData: App = await this.appService.findAppById(appId);
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // const appId: string
-      //     // Name: abc
-      //   }
-      // ];
 
       //return
       res.status(200).json({ data: x, message: 'findOne' });
@@ -57,13 +57,6 @@ class AppController {
     try {
       //const appData: CreateUserDto = req.body;
       //const createAppData: App = await this.appService.createApp(appData);
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // appData
-      //   }
-      // ];
 
       res.status(201).json({ data: x, message: 'created' });
     } catch (error) {
@@ -80,14 +73,6 @@ class AppController {
       //const appId: string = req.params.id;
       //const appLicense: CreateUserDto = req.body;
       //const createAppLicense: App = await this.appService.createlicenseApp(appId, appLicense)
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // appId
-      //     // appLicense
-      //   }
-      // ];
 
       //return
       res.status(201).json({ data: x, message: 'created License' });
@@ -105,14 +90,6 @@ class AppController {
       //const appId: string = req.params.id;
       //const Licenseprice: string = req.params.price;
       //const getAppLicenseprice: App = await this.appService.getAppLicenseprice(appId, Licenseprice)
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // appId
-      //     // Licenseprice
-      //   }
-      // ];
 
       //return
       res.status(201).json({ data: x, message: 'got license' });
@@ -128,13 +105,6 @@ class AppController {
   ) => {
     try {
       //const getallAppLicense: App = await this.appService.findAllLicense()
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // getallAppLicense
-      //   }
-      // ];
 
       //return
       res.status(201).json({ data: x, message: 'findLicense' });
@@ -152,14 +122,6 @@ class AppController {
       //const appId: string = req.params.id;
       //const purchaseLicense: string = req.body;
       //const getpurchaseLicense: App = await this.appService.purchaseLicense(appId, purchaseLicense)
-      const x: App = { appId: '1' };
-      // [
-      //   {
-      //     /// declare app input
-      //     // appId
-      //     // purchaseLicense
-      //   }
-      // ];
 
       //return
       res.status(201).json({ data: x, message: 'purchased License' });
--- server/src/interfaces/users.interface.ts
+++ server/src/interfaces/users.interface.ts
@@ -1,5 +1,6 @@
 export interface User {
-  //_id: string;
+  _id: string;
   email: string;
-  //password: string;
+  password: string;
+  userType: string;
 }
