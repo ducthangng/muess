f7adf86cacff7634bb7f5fc1b53026382a4ebb76
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-10-25 19:58:42 +0700

    Create users route backend

--- server/src/controllers/mockapp.controller.ts
+++ server/src/controllers/mockapp.controller.ts
@@ -3,6 +3,8 @@ import { CreateUserDto } from '@dtos/users.dto';
 import { App } from '@interfaces/apps.interface';
 //import appService from '@services/apps.service';
 
+import { APISmartContract } from '@/dtos/smartContract.dto';
+
 const x: App = {
   _id: '1',
   title: '1',
@@ -20,24 +22,15 @@ const x: App = {
   ]
 };
 
-const createlicenseApp: App = {
-  _id: '1',
-  title: '1',
-  description: '1',
-  rated: '1',
-  reviewer: '1',
-  appTag: '1',
-  downloaded: 1,
-  appInformations: '1',
-  feedbacks: [
-    {
-      name: '1',
-      content: '1'
-    }
-  ]
+const smartContractMockData: APISmartContract = {
+  productId: '1',
+  provider: 'Thang',
+  price: 1.76,
+  purchaseDate: new Date('2023-10-10'),
+  smartContractInfo: '1'
 };
 
-class AppController {
+class AppsController {
   //public appService = new appService();
 
   public getApps = async (req: Request, res: Response, next: NextFunction) => {
@@ -95,7 +88,7 @@ class AppController {
       //return
       res
         .status(201)
-        .json({ data: createlicenseApp, message: 'created License' });
+        .json({ data: smartContractMockData, message: 'created License' });
     } catch (error) {
       next(error);
     }
@@ -112,7 +105,9 @@ class AppController {
       //const getAppLicenseprice: App = await this.appService.getAppLicenseprice(appId, Licenseprice)
 
       //return
-      res.status(201).json({ data: x, message: 'got license' });
+      res
+        .status(201)
+        .json({ data: smartContractMockData, message: 'got license' });
     } catch (error) {
       next(error);
     }
@@ -127,7 +122,9 @@ class AppController {
       //const getallAppLicense: App = await this.appService.findAllLicense()
 
       //return
-      res.status(201).json({ data: x, message: 'findLicense' });
+      res
+        .status(201)
+        .json({ data: smartContractMockData, message: 'findLicense' });
     } catch (error) {
       next(error);
     }
@@ -144,9 +141,13 @@ class AppController {
       //const getpurchaseLicense: App = await this.appService.purchaseLicense(appId, purchaseLicense)
 
       //return
-      res.status(201).json({ data: x, message: 'purchased License' });
+      res
+        .status(201)
+        .json({ data: smartContractMockData, message: 'purchased License' });
     } catch (error) {
       next(error);
     }
   };
 }
+
+export default AppsController;
