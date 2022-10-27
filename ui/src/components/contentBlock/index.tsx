ed3c1bc5a41607cc760c8b2b1ea084a808ec56dd
LocBuiNhien <15635@student.vgu.edu.vn>
2022-10-27 17:28:20 +0700

    fix landing page and add login + register

--- ui/src/components/contentBlock/index.tsx
+++
@@ -1,9 +0,0 @@
-import LeftContentBlock from '../../components/contentBlock/leftContent';
-import { ContentBlockProps } from '../../components/contentBlock/styles';
-
-const ContentBlock = (props: ContentBlockProps) => {
-  if (props.type === 'left') return <LeftContentBlock {...props} />;
-  return null;
-};
-
-export default ContentBlock;
