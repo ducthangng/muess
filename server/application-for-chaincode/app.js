835587658cc4ae252ab26056e05b1b14c1ead4de
Nghi Hua <huanhatgianghi@gmail.com>
2022-10-27 21:20:03 +0700

    create functions for interacting with transactions

--- server/application-for-chaincode/app.js
+++ server/application-for-chaincode/app.js
@@ -4,6 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// how to run this thing
+// go to test-network, run
+//    ./network.sh down
+//    ./network.sh up createChannel -ca -s couchdb
+//    ./network.sh deployCC -ccn muess -ccp ../chaincode/ -ccl javascript
+// go to this directory (application-for-chaincode)
+//    npm install
+//    node app.js
+// common errors:
+//  - "appUser not found/already exists"
+//    -> change userId below to sth else and retry node app.js
+//  - "failed to register user/identity not found"
+//    -> delete wallet folder and retry node app.js
+
 "use strict";
 
 const { Gateway, Wallets } = require("fabric-network");
@@ -13,70 +27,114 @@ const {
   buildCAClient,
   registerAndEnrollUser,
   enrollAdmin,
-} = require("../../test-application/javascript/CAUtil.js");
+} = require("./CAUtil.js");
 const {
   buildCCPOrg1,
   buildWallet,
-} = require("../../test-application/javascript/AppUtil.js");
+} = require("./AppUtil.js");
 
 const channelName = "mychannel";
-const chaincodeName = "ledger";
+const chaincodeName = "muess";
 const mspOrg1 = "Org1MSP";
 
 const walletPath = path.join(__dirname, "wallet");
 const userId = "appUser";
 
+
+// utility functions
 function prettyJSONString(inputString) {
   return JSON.stringify(JSON.parse(inputString), null, 2);
 }
 
-// pre-requisites:
-// - fabric-sample two organization test-network setup with two peers, ordering service,
-//   and 2 certificate authorities, with the state database using couchdb
-//         ===> from directory /fabric-samples/test-network
-//         ./network.sh up createChannel -ca -s couchdb
-// - Use any of the asset-transfer-ledger-queries chaincodes deployed on the channel "mychannel"
-//   with the chaincode name of "ledger". The following deploy command will package,
-//   install, approve, and commit the javascript chaincode, all the actions it takes
-//   to deploy a chaincode to a channel.
-//         ===> from directory /fabric-samples/test-network
-//         ./network.sh deployCC -ccn ledger -ccp ../asset-transfer-ledger-queries/chaincode-javascript/ -ccl javascript
-// - Be sure that node.js is installed
-//         ===> from directory /fabric-samples/asset-transfer-ledger-queries/application-javascript
-//         node -v
-// - npm installed code dependencies
-//         ===> from directory /fabric-samples/asset-transfer-ledger-queries/application-javascript
-//         npm install
-// - to run this test application
-//         ===> from directory /fabric-samples/asset-transfer-ledger-queries/application-javascript
-//         node app.js
+// functions to call on transactions
+const createProposal = async (contract, proposalID, appID, buyerID, proposedPrice, licenseDetails) => {
+  console.log("create a proposal");
+  await contract.submitTransaction(
+    "CreateProposal",
+    proposalID,
+    appID,
+    buyerID,
+    proposedPrice,
+    licenseDetails
+  );
+}
 
-// NOTE: If you see  kind an error like these:
-/*
-    2020-08-07T20:23:17.590Z - error: [DiscoveryService]: send[mychannel] - Channel:mychannel received discovery error:access denied
-    ******** FAILED to run the application: Error: DiscoveryService: mychannel error: access denied
+const acceptProposal = async (contract, licenseID, creatorID, proposalID) => {
+  console.log("accept a proposal");
+  await contract.submitTransaction(
+    "AcceptProposal",
+    licenseID,
+    creatorID,
+    proposalID
+  );
+}
 
-   OR
+const readAsset = async (contract, assetID) => {
+  console.log(
+    "\nread asset"
+  );
+  const result = await contract.evaluateTransaction("ReadAsset", assetID);
+  // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+  return result;
+}
 
-   Failed to register user : Error: fabric-ca request register failed with errors [[ { code: 20, message: 'Authentication failure' } ]]
-   ******** FAILED to run the application: Error: Identity not found in wallet: appUser
-*/
-// Delete the /fabric-samples/asset-transfer-ledger-queries/application-javascript/wallet directory
-// and retry this application.
-//
-// The certificate authority must have been restarted and the saved certificates for the
-// admin and application user are not valid. Deleting the wallet store will force these to be reset
-// with the new certificate authority.
-//
+const queryLicensesByAppID = async (contract, appID) => {
+  console.log(
+    "\n--> Evaluate Transaction: QueryLicensesByAppID"
+  );
+  const result = await contract.evaluateTransaction(
+    "QueryLicensesByAppID",
+    appID
+  );
+  return result;
+}
 
-/**
- *  A test application to show ledger queries operations with any of the asset-transfer-ledger-queries chaincodes
- *   -- How to submit a transaction
- *   -- How to query and check the results
- *
- * To see the SDK workings, try setting the logging to show on the console before running
- *        export HFC_LOGGING='{"debug":"console"}'
- */
+const queryLicensesByOwnerID = async (contract, ownerID) => {
+  console.log(
+    "\n--> Evaluate Transaction: QueryLicensesByOwnerID"
+  );
+  const result = await contract.evaluateTransaction(
+    "QueryLicensesByOwnerID",
+    ownerID
+  );
+  return result;
+  // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+}
+
+const queryLicensesByCreatorID = async (contract, creatorID) => {
+  console.log(
+    "\n--> Evaluate Transaction: QueryLicensesByCreatorID"
+  );
+  const result = await contract.evaluateTransaction(
+    "QueryLicensesByCreatorID",
+    creatorID
+  );
+  return result;
+  // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+}
+
+const queryProposalsByAppID = async (contract, appID) => {
+  console.log(
+    "\n--> Evaluate Transaction: QueryProposalsByAppID"
+  );
+  const result = await contract.evaluateTransaction(
+    "QueryProposalsByAppID",
+    appID
+  );
+  return result;
+}
+
+const queryProposalsByBuyerID = async (contract, buyerID) => {
+  console.log(
+    "\n--> Evaluate Transaction: QueryProposalsByBuyerID"
+  );
+  const result = await contract.evaluateTransaction(
+    "QueryProposalsByBuyerID",
+    buyerID
+  );
+  return result;
+}
+
 async function main() {
   let skipInit = false;
   if (process.argv.length > 2) {
@@ -131,140 +189,46 @@ async function main() {
 
       // Build a network instance based on the channel where the smart contract is deployed
       const network = await gateway.getNetwork(channelName);
-      let result;
 
       // Get the contract from the network.
       const contract = network.getContract(chaincodeName);
 
-      // Now let's try to submit a transaction.
-      // This will be sent to both peers and if both peers endorse the transaction, the endorsed proposal will be sent
-      // to the orderer to be committed by each of the peer's to the channel ledger.
 
-      const proposalID = "proposal4";
-      const licenseID = "license4";
 
-      console.log("create a proposal");
-      result = await contract.submitTransaction(
-        "CreateProposal",
-        proposalID,
-        "app0",
-        "nghihua",
-        100,
-        "free use"
-      );
 
-      console.log(
-        "\n--> Evaluate Transaction: ReadAsset, function returns information about an asset with ID(proposal1)"
-      );
-      result = await contract.evaluateTransaction("ReadAsset", proposalID);
+      /* MODIFY THIS PART TO TEST OUT CHAINCODE */
+      ///////////////////////////////////////////
+      // some consts
+      const proposalID = "proposal3";
+      const licenseID = "license3";
+      const appID = "app3";
+      const creatorID = "billgates";
+      const buyerID = "nghihua";
+
+      let result;
+
+      await createProposal(contract, proposalID, appID, buyerID, 100, "free use");
+
+      // read proposal we just created
+      result = await readAsset(contract, proposalID);
       console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
-      console.log(
-        '\n--> Evaluate Transaction: AssetExists, function returns "true" if an asset with ID(proposal1) exist'
-      );
-      result = await contract.evaluateTransaction("AssetExists", proposalID);
+      await acceptProposal(contract, licenseID, creatorID, proposalID);
+
+      // read the proposal again, which will return an error
+      // because the proposal is deleted once it has been accepted
+      result = await readAsset(contract, proposalID);
       console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
-      // Let's try a query operation (function).
-      // This will be sent to just one peer and the results will be shown.
-      // console.log('\n--> Evaluate Transaction: GetAssetsByRange, function returns assets in a specific range from proposal1 to before asset6');
-      // result = await contract.evaluateTransaction('GetAssetsByRange', 'proposal1', 'asset6');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // console.log('\n--> Evaluate Transaction: GetAssetsByRange, function use an open start and open end range to return assest1 to asset6');
-      // result = await contract.evaluateTransaction('GetAssetsByRange', '', '');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // console.log('\n--> Evaluate Transaction: GetAssetsByRange, function use an fixed start (asset3) and open end range to return assest3 to asset6');
-      // result = await contract.evaluateTransaction('GetAssetsByRange', 'asset3', '');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // console.log('\n--> Evaluate Transaction: GetAssetsByRange, function use an open start and fixed end (asset3) range to return assest1 to asset2');
-      // result = await contract.evaluateTransaction('GetAssetsByRange', '', 'asset3');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // Now let's try to submit a transaction that deletes an asset
-      // This will be sent to both peers and if both peers endorse the transaction, the endorsed proposal will be sent
-      // to the orderer to be committed by each of the peer's to the channel ledger.
-      // console.log('\n--> Submit Transaction: DeleteAsset with ID(asset7)');
-      // await contract.submitTransaction('DeleteAsset', 'asset7');
-      // console.log('*** Result: committed');
-
-      // console.log('\n--> Evaluate Transaction: AssetExists, function returns "false" if an asset with ID(asset7) does not exist');
-      // result = await contract.evaluateTransaction('AssetExists', 'asset7');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // console.log('\n--> Submit Transaction: TransferAsset, transfer asset(asset2) to new owner(Max)');
-      // await contract.submitTransaction('TransferAsset', 'asset2', 'Max');
-      // console.log('*** Result: committed');
-
-      // console.log('\n--> Evaluate Transaction: ReadAsset, function returns information about an asset with ID(asset2)');
-      // result = await contract.evaluateTransaction('ReadAsset', 'asset2');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // Rich Query with Pagination (Only supported if CouchDB is used as state database)
-      // console.log('\n--> Evaluate Transaction: QueryAssetsWithPagination, function returns "Max" assets');
-      // result = await contract.evaluateTransaction('QueryAssetsWithPagination', '{"selector":{"docType":"asset","owner":"Max"}, "use_index":["_design/indexOwnerDoc", "indexOwner"]}', '1', '');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // Recover the bookmark from previous query. Normally it will be inside a variable.
-      // const resultJson = JSON.parse(result.toString());
-
-      // console.log('\n--> Evaluate Transaction: QueryAssetsWithPagination, function returns "Max" assets next page');
-      // result = await contract.evaluateTransaction('QueryAssetsWithPagination', '{"selector":{"docType":"asset","owner":"Max"}, "use_index":["_design/indexOwnerDoc", "indexOwner"]}', '1', resultJson.bookmark);
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // Rich Query (Only supported if CouchDB is used as state database):
-      console.log(
-        "\n--> Evaluate Transaction: QueryProposalsByBuyerID, find all proposal assets with buyerID(nghihua)"
-      );
-      result = await contract.evaluateTransaction(
-        "QueryProposalsByBuyerID",
-        "nghihua"
-      );
+      // read the license we just created
+      result = await readAsset(contract, proposalID);
       console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
-      console.log("accept the proposal");
-      result = await contract.submitTransaction(
-        "AcceptProposal",
-        licenseID,
-        "billgates",
-        proposalID
-      );
-      console.log(result);
-
-      console.log(
-        "\n--> Evaluate Transaction: QueryLicensesByOwnerID, find all license assets with ownerID(nghihua)"
-      );
-      result = await contract.evaluateTransaction(
-        "QueryLicensesByOwnerID",
-        "nghihua"
-      );
+      // read the license with appID specified above
+      result = await queryLicensesByAppID(contract, appID);
       console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
-      // console.log('\n--> Evaluate Transaction: GetAssetHistory, get the history of an asset(asset7)');
-      // result = await contract.evaluateTransaction('GetAssetHistory', 'asset7');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
-      // // Rich Query (Only supported if CouchDB is used as state database):
-      // console.log('\n--> Evaluate Transaction: QueryAssets, assets of size 15');
-      // result = await contract.evaluateTransaction('QueryAssets', '{"selector":{"size":15}}');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // // Rich Query with index design doc and index name specified (Only supported if CouchDB is used as state database):
-      // console.log('\n--> Evaluate Transaction: QueryAssets, Jin Soo\'s assets');
-      // result = await contract.evaluateTransaction('QueryAssets', '{"selector":{"docType":"asset","owner":"Jin Soo"}, "use_index":["_design/indexOwnerDoc", "indexOwner"]}');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // // Range Query with Pagination
-      // console.log('\n--> Evaluate Transaction: GetAssetsByRangeWithPagination - get page 1 of assets from asset2 to asset6 (asset2, asset3)');
-      // result = await contract.evaluateTransaction('GetAssetsByRangeWithPagination', 'asset2', 'asset6', '2', '');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-      // // Range Query with Pagination
-      // console.log('\n--> Evaluate Transaction: GetAssetsByRangeWithPagination - get page 2 of assets from asset2 to asset6 (asset4, asset5)');
-      // result = await contract.evaluateTransaction('GetAssetsByRangeWithPagination', 'asset2', 'asset6', '2', 'asset4');
-      // console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
       console.log("*** all tests completed");
     } finally {
