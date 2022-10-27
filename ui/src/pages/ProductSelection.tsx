c84d847a79ab606cba27937922a9dff9bccec4b3
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-10-27 20:40:19 +0700

    edit product page

--- ui/src/pages/ProductSelection.tsx
+++ ui/src/pages/ProductSelection.tsx
@@ -14,80 +14,99 @@ function ProductSelection() {
   });
 
   return (
-    <div>
-      <SideMenu />
-      <body>
-        <div
-          style={{
-            display: 'flex'
-          }}
-        >
-          <Divider className="" orientation="left">
-            FEATURE APPS:
-          </Divider>
-          <Divider className="" orientation="left">
-            <div>
-              <Button
-                type="primary"
-                shape="default"
-                size="middle"
-                href="/productselection"
-              >
-                Featured Apps
-              </Button>
-              <Button
-                type="primary"
-                shape="default"
-                size="middle"
-                href="/productselection"
-              >
-                Owned Apps
-              </Button>
-              <Button
-                type="primary"
-                shape="default"
-                size="middle"
-                href="/productselection"
-              >
-                My Apps
-              </Button>
-            </div>
-          </Divider>
+    <>
+      <div>
+        <SideMenu />
+        <body>
+          <div
+            style={{
+              width: '100%'
+            }}
+          >
+            <Divider
+              className=""
+              orientation="left"
+              style={{
+                fontSize: '48px',
+                fontWeight: 'bold',
+                paddingTop: 100,
+                paddingBottom: 5,
+                paddingLeft: 350,
+                color: '3A001E'
+              }}
+            >
+              FEATURE APPS
+            </Divider>
+            <Divider
+              className=""
+              orientation="left"
+              style={{
+                paddingLeft: 370
+              }}
+            >
+              <div>
+                <Button
+                  ghost
+                  shape="default"
+                  size="middle"
+                  href="/productselection"
+                >
+                  Featured Apps
+                </Button>
+                <Button
+                  ghost
+                  shape="default"
+                  size="middle"
+                  href="/productselection"
+                >
+                  Owned Apps
+                </Button>
+                <Button
+                  ghost
+                  shape="default"
+                  size="middle"
+                  href="/productselection"
+                >
+                  My Apps
+                </Button>
+              </div>
+            </Divider>
 
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-md md:max-w-6xl mx-auto">
-            {data &&
-              data.map((item) => {
-                return (
-                  <MockProduct
-                    _id={item._id}
-                    title={item.title}
-                    description={item.description}
-                    creatorId={item.creatorId}
-                    creatorName={item.creatorName}
-                    rated={item.rated}
-                    appType={item.appType}
-                    appPaymentMethod={item.appPaymentMethod}
-                    appCategories={item.appCategories}
-                    reviewer={item.reviewer}
-                    downloaded={item.downloaded}
-                    appIcon={item.appIcon} // link to database.
-                    feedbacks={item.feedbacks}
-                    appTags={item.appTags}
-                  />
-                );
-              })}
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-md md:max-w-6xl mx-auto">
+              {data &&
+                data.map((item) => {
+                  return (
+                    <MockProduct
+                      _id={item._id}
+                      title={item.title}
+                      description={item.description}
+                      creatorId={item.creatorId}
+                      creatorName={item.creatorName}
+                      rated={item.rated}
+                      appType={item.appType}
+                      appPaymentMethod={item.appPaymentMethod}
+                      appCategories={item.appCategories}
+                      reviewer={item.reviewer}
+                      downloaded={item.downloaded}
+                      appIcon={item.appIcon} // link to database.
+                      feedbacks={item.feedbacks}
+                      appTags={item.appTags}
+                    />
+                  );
+                })}
+            </div>
+            <br></br>
+            <Row justify="center">
+              <Pagination
+                defaultCurrent={1}
+                total={50}
+                style={{ color: '#8172D5' }}
+              />
+            </Row>
           </div>
-          <br></br>
-          <Row justify="center">
-            <Pagination
-              defaultCurrent={1}
-              total={50}
-              style={{ color: '#8172D5' }}
-            />
-          </Row>
-        </div>
-      </body>
-    </div>
+        </body>
+      </div>
+    </>
   );
 }
 
