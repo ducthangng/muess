e146ed49f7a6cbb34863978357048381106cc54c
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-10-31 08:44:58 +0700

    Product License

--- server/src/routes/license.route.ts
+++ server/src/routes/license.route.ts
@@ -13,37 +13,17 @@ class HLFRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.post(
-      `${this.path}/create-app`,
-      authMiddleware,
-      this.licenseController.createApp
-    );
-    this.router.post(
-      `${this.path}/create-proposal`,
-      authMiddleware,
-      this.licenseController.createProposal
-    );
-    this.router.post(
-      `${this.path}/accept-proposal`,
-      authMiddleware,
-      this.licenseController.acceptProposal
-    );
-    this.router.post(
-      `${this.path}/reject-proposal`,
-      authMiddleware,
-      this.hlfController.rejectProposal
-    );
     // remember to encodeURIComponent(creatorId) before sending to server
-    this.router.get(
-      `${this.path}/apps-by-creator/:creatorId`,
-      authMiddleware,
-      this.licenseController.getAppsByCreatorId
-    );
-    this.router.get(
-      `${this.path}/proposals-by-app/:appId`,
-      authMiddleware,
-      this.licenseController.getProposalsByAppId
-    );
+    // this.router.get(
+    //   `${this.path}/apps-by-creator/:creatorId`,
+    //   authMiddleware,
+    //   this.licenseController.getAppsByCreatorId
+    // // );
+    // this.router.get(
+    //   `${this.path}/proposals-by-app/:appId`,
+    //   authMiddleware,
+    //   this.licenseController.getProposalsByAppId
+    // );
     this.router.get(
       `${this.path}/proposals-by-buyer/:buyerId`,
       authMiddleware,
---
+++ ui/src/pages/ProductLicense.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react';
+import MockProduct from '../components/MockProduct';
+import SideMenu from '../components/Header/SideMenu';
+import { Row, Divider } from 'antd';
+import { Pagination } from 'antd';
+import { Button } from 'antd';
+import { appApi } from '../api/appApi';
+import { useQuery } from 'react-query';
+
+function ProductLicense() {
+  const { data } = useQuery(['productLicense'], async () => {
+    return await appApi.getOwnedApps();
+  });
+
+  return (
+    <>
+      <div>
+        <SideMenu />
+        <body>
+          <div
+            style={{
+              width: '100%'
+            }}
+          >
+            <Divider
+              className=""
+              orientation="left"
+              style={{
+                fontSize: '48px',
+                fontWeight: 'bold',
+                paddingTop: 100,
+                paddingBottom: 5,
+                paddingLeft: 350,
+                color: '3A001E'
+              }}
+            >
+              OWNED APPS
+            </Divider>
+            <Divider
+              className=""
+              orientation="left"
+              style={{
+                paddingLeft: 370
+              }}
+            >
+              <div>
+                <Button
+                  ghost
+                  shape="default"
+                  size="middle"
+                  href="/productSelection"
+                >
+                  Featured Apps
+                </Button>
+                <Button ghost shape="default" size="middle" href="/ownedapps">
+                  Owned Apps
+                </Button>
+              </div>
+            </Divider>
+
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-md md:max-w-6xl mx-auto">
+              {data &&
+                data.map((item) => {
+                  return (
+                    <MockProduct
+                      _id={item._id}
+                      title={item.title}
+                      description={item.description}
+                      creatorId={item.creatorId}
+                      creatorName={item.creatorName}
+                      rated={item.rated}
+                      appType={item.appType}
+                      appPaymentMethod={item.appPaymentMethod}
+                      appCategories={item.appCategories}
+                      reviewer={item.reviewer}
+                      downloaded={item.downloaded}
+                      appIcon={item.appIcon} // link to database.
+                      feedbacks={item.feedbacks}
+                      appTags={item.appTags}
+                      imageSrc={item.imageSrc}
+                    />
+                  );
+                })}
+            </div>
+            <br></br>
+            <Row justify="center">
+              <Pagination
+                defaultCurrent={1}
+                total={50}
+                style={{ color: '#8172D5' }}
+              />
+            </Row>
+          </div>
+        </body>
+      </div>
+    </>
+  );
+}
+
+export default ProductLicense;
